89580a314b9fa38a89c41e4cf268d3c1651b23f0
UnstoppableMango <erik.rasmussen@unmango.dev>
2025-11-07 22:55:17 -0600

    WIP

--- cmd/openapi2ts/main.go
+++ cmd/openapi2ts/main.go
@@ -1,7 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/unstoppablemango/ux/pkg/plugin/decl"
+)
 
 func main() {
+	plugin := decl.Cli{
+		Name: "",
+	}
+
 	fmt.Println("Hello world")
 }
---
+++ pkg/openapi-typescript.go
@@ -0,0 +1,9 @@
+package a2b
+
+import (
+	"github.com/unstoppablemango/ux/pkg/plugin"
+)
+
+var OpenApiTypeScript = plugin.Cli{
+	Name: "openapi-typescript",
+}
