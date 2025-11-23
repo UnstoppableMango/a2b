a69fbceb6093df3d3433a55a309ffbb898f0e6a6
UnstoppableMango <erik.rasmussen@unmango.dev>
2025-11-22 23:02:14 -0600

    Flake parts

--- flake.nix
+++ flake.nix
@@ -3,80 +3,75 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
+    flake-parts.url = "github:hercules-ci/flake-parts";
     treefmt-nix = {
       url = "github:numtide/treefmt-nix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     gomod2nix = {
       url = "github:nix-community/gomod2nix";
-      inputs = {
-        nixpkgs.follows = "nixpkgs";
-        flake-utils.follows = "flake-utils";
-      };
+      inputs.nixpkgs.follows = "nixpkgs";
     };
     ux = {
       url = "github:unstoppablemango/ux";
       inputs = {
         nixpkgs.follows = "nixpkgs";
-        flake-utils.follows = "flake-utils";
         gomod2nix.follows = "gomod2nix";
-        treefmt-nix.follows = "treefmt-nix";
       };
     };
   };
 
   outputs =
-    {
-      self,
-      nixpkgs,
-      flake-utils,
-      treefmt-nix,
-      gomod2nix,
-      ux,
-    }:
-    flake-utils.lib.eachDefaultSystem (
-      system:
-      let
-        pkgs = nixpkgs.legacyPackages.${system};
-        a2b = pkgs.callPackage ./. {
-          inherit (gomod2nix.legacyPackages.${system}) buildGoApplication;
-        };
-      in
-      {
-        formatter = treefmt-nix.lib.mkWrapper pkgs {
-          projectRootFile = "flake.nix";
-          programs = {
-            gofmt.enable = true;
-            nixfmt.enable = true;
-            dprint.enable = true;
+    inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+      imports = [ inputs.treefmt-nix.flakeModule ];
+      perSystem =
+        { pkgs, system, ... }:
+        let
+          a2b = pkgs.callPackage ./. {
+            inherit (inputs.gomod2nix.legacyPackages.${system}) buildGoApplication;
+          };
+        in
+        {
+          apps.ux = inputs.ux.apps.${system}.ux;
+          apps.openapi2ts = {
+            type = "app";
+            program = a2b + "/bin/openapi2ts";
+            meta = {
+              description = "Convert OpenAPI specs to TypeScript types";
+              homepage = "https://github.com/UnstoppableMango/a2b";
+              license = [ pkgs.lib.licenses.mit ];
+              maintainers = [
+                {
+                  name = "Erik Rasmussen";
+                  email = "erik.rasmussen@unmango.dev";
+                }
+              ];
+            };
+          };
+
+          packages.a2b = a2b;
+          packages.default = a2b;
+
+          devShells.default = pkgs.callPackage ./shell.nix {
+            inherit (inputs.gomod2nix.legacyPackages.${system}) mkGoEnv gomod2nix;
+            inherit (inputs.ux.packages.${system}) ux;
+          };
+
+          treefmt = {
+            projectRootFile = "flake.nix";
+            programs = {
+              gofmt.enable = true;
+              nixfmt.enable = true;
+              # dprint.enable = true;
+            };
           };
         };
-
-        apps.ux = ux.apps.${system}.ux;
-        apps.openapi2ts = {
-          type = "app";
-          program = a2b + "/bin/openapi2ts";
-          meta = {
-            description = "Convert OpenAPI specs to TypeScript types";
-            homepage = "https://github.com/UnstoppableMango/a2b";
-            license = [ pkgs.lib.licenses.mit ];
-            maintainers = [
-              {
-                name = "Erik Rasmussen";
-                email = "erik.rasmussen@unmango.dev";
-              }
-            ];
-          };
-        };
-
-        packages.a2b = a2b;
-        packages.default = a2b;
-
-        devShells.default = pkgs.callPackage ./shell.nix {
-          inherit (gomod2nix.legacyPackages.${system}) mkGoEnv gomod2nix;
-          inherit (ux.packages.${system}) ux;
-        };
-      }
-    );
+    };
 }
