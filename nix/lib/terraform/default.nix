3f39fc85730d596e34875d8f9c6f8fbd28cd88b6
UnstoppableMango <erik.rasmussen@unmango.dev>
2026-06-11 22:36:35 -0500

    Shorter?

--- nix/lib/terraform/default.nix
+++ nix/lib/terraform/default.nix
@@ -4,10 +4,8 @@
     attrs:
     import ./plugin-codegen-openapi.nix (
       {
-        lib = attrs.lib or pkgs.lib;
-        runCommand = attrs.runCommand or pkgs.runCommand;
-        terraform-plugin-codegen-openapi =
-          attrs.terraform-plugin-codegen-openapi or terraform-plugin-codegen-openapi;
+        inherit (pkgs) lib runCommand;
+        inherit terraform-plugin-codegen-openapi;
       }
       // attrs
     );
