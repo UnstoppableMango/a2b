b3bd5c627c230283f87f643a743235c5e2de8a2c
UnstoppableMango <erik.rasmussen@unmango.dev>
2026-06-16 20:49:46 -0500

    GitOps toolkit components, mimicing flux bootstrap

--- nix/lib/flux/default.nix
+++ nix/lib/flux/default.nix
@@ -6,6 +6,7 @@ let
     install = callPackage ./install.nix;
     createSourceGit = callPackage ./create-source-git.nix;
     createKustomization = callPackage ./create-kustomization.nix;
+    gotkComponents = callPackage ./gotk-components.nix;
   };
 in
 packages
