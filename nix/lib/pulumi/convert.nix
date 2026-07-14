94ba60278c41de1984b1c42cf2d4940431cb806a
UnstoppableMango <erik.rasmussen@unmango.dev>
2026-07-13 23:30:45 -0500

    WIP filling in missing language plugins

--- nix/lib/pulumi/convert.nix
+++ nix/lib/pulumi/convert.nix
@@ -10,14 +10,19 @@
   generateOnly ? true,
   language,
   name,
+  plugins ? [ ],
   src,
 }:
 
 # https://www.pulumi.com/docs/iac/cli/commands/pulumi_convert/
+# `plugins` should contain any `pulumi-converter-<from>` binaries needed for
+# the source ecosystem (none are packaged in nixpkgs) plus, if `--generate-only`
+# is disabled, the target `pulumi-language-<language>` plugin.
 runCommand name env ''
   runHook preRun
 
   export HOME="$(mktemp -d)"
+  export PATH="${lib.makeBinPath plugins}:$PATH"
   cp -r ${lib.escapeShellArg (toString src)} ./src
   chmod -R u+w ./src
 
--- nix/lib/pulumi/gen-sdk.nix
+++ nix/lib/pulumi/gen-sdk.nix
@@ -1,6 +1,7 @@
 {
   lib,
   pulumi,
+  pulumiPackages,
   runCommand,
 }:
 {
@@ -9,15 +10,32 @@
   language ? null,
   languages ? [ ],
   name,
+  plugins ? [ ],
   schema,
   version ? null,
 }:
-
+let
+  # Known nixpkgs pulumi language plugins; languages without one (dotnet, java, yaml, ...)
+  # must be supplied via `plugins`.
+  knownLanguagePlugins = {
+    go = pulumiPackages.pulumi-go;
+    nodejs = pulumiPackages.pulumi-nodejs;
+    python = pulumiPackages.pulumi-python;
+  };
+  requestedLanguages = lib.optional (language != null) language ++ languages;
+  languagePlugins = map (
+    lang:
+    knownLanguagePlugins.${lang} or (throw ''
+      pulumi.genSdk: no nixpkgs plugin known for language "${lang}"; pass it via `plugins`
+    '')
+  ) requestedLanguages;
+in
 # https://www.pulumi.com/docs/iac/cli/commands/pulumi_package_gen-sdk/
 runCommand name env ''
   runHook preRun
 
   export HOME="$(mktemp -d)"
+  export PATH="${lib.makeBinPath (languagePlugins ++ plugins)}:$PATH"
 
   ${pulumi}/bin/pulumi package gen-sdk \
     --out "$out" \
